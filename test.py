63199ca594259999cd1230abbe190f684db5505a
rufusnufus <nufusrufus@gmail.com>
2020-04-24 20:28:58 +0300

    add files

---
+++ test.py
@@ -0,0 +1,24 @@
+import spacy
+import random
+import sys
+
+try:
+	with open("input.txt") as fin:
+		lines = fin.read().splitlines()
+		input_line = random.choice(lines)
+except FileNotFoundError:
+	print("input.txt doesn't exist")
+	sys.exit()
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(input_line)
+
+names = []
+for ent in doc.ents:
+	if ent.label_ == "PERSON":
+		name = ent.text.replace("'s","")
+		names.append(name)
+
+with open("output.txt", "w+") as fout:
+	fout.write(f'I: {input_line}\nO: ')
+	fout.write(', '.join(names))
